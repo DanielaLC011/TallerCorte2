f92f9e8003ecd4c526fafb3529770128b54c8d53
DanielaLC011 <101845416+DanielaLC011@users.noreply.github.com>
2022-05-30 20:53:33 -0500

    Punto3
    se agregan comentarios

--- Taller2/src/TallerCorte2/Punto3.java
+++ Taller2/src/TallerCorte2/Punto3.java
@@ -14,15 +14,19 @@ public class Punto3 {
     
     public static void main(String[] args) {
         
+        //Se declaran variables 
         int radio, altura; 
         float pi = 3.1416f, area, volumen;
         
+        //se solicitan datos al usuario (radio y volumen)
         radio = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el radio"));
         altura = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la altura"));
         
+        //Se agrega la formula para sacar el area y mostrarlo
         area = 2*pi*radio*(radio+altura);
         JOptionPane.showMessageDialog(null, "El area del cilindro es:" +area);
         
+        //Se agrega la formula para sacar el volumen y mostrarlo
         volumen = pi*(radio*radio)*altura;
         JOptionPane.showMessageDialog(null, "El volumen del cilindro es:" +volumen);
         
