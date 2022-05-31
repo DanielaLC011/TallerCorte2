771a53887c7d71f8b4b86f0260b7d9633c45eb83
DanielaLC011 <101845416+DanielaLC011@users.noreply.github.com>
2022-05-30 20:46:11 -0500

    Punto4
    Se agregan comentarios

--- Taller2/src/TallerCorte2/parImpar.java
+++ Taller2/src/TallerCorte2/parImpar.java
@@ -14,12 +14,15 @@ public class parImpar {
     
     public static void main(String[] args) {
         
+        //Se declaran las variables
         int num;
         Scanner leer = new Scanner (System.in);
         
+        //Se pide al usuario ingresar numero
         System.out.println("Ingrese su numero");
         num = leer.nextInt(); 
         
+        //Se crea el ciclo para comprobar y mostrar si e numero es par o impar
         if (num % 2 == 0)
             System.out.print("El numero es par");
         else 
