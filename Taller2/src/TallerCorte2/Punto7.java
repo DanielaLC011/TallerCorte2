cad4e325586b9bf6704702623f3325e8913d2fea
DanielaLC011 <101845416+DanielaLC011@users.noreply.github.com>
2022-05-30 19:26:18 -0500

    Punto 7
    Se crea programa para determinar cual de los 3 numeros, es menor.

---
+++ Taller2/src/TallerCorte2/Punto7.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TallerCorte2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Punto7 {
+    public static void main(String[] args) {
+        
+        int num1, num2, num3; 
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero"));
+        num3 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el tercer numero"));
+        
+        if (num1<num2 && num1<num3){
+            JOptionPane.showMessageDialog(null, "El numero menor es:" +num1);
+        }
+        else if (num2<num1 && num2<num3){
+            JOptionPane.showMessageDialog(null, "El numero menor es:" +num2);
+        }
+        else if (num3<num1 && num3<num2){
+            JOptionPane.showMessageDialog(null, "El numero menor es:" +num3);
+        }
+    }
+}
