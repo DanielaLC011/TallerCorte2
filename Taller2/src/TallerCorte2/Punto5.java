8d452ccdfc8209d535c1e1ba46de6a11991937fe
DanielaLC011 <101845416+DanielaLC011@users.noreply.github.com>
2022-05-30 20:55:05 -0500

    Punto5
    se agregan comentarios

--- Taller2/src/TallerCorte2/Punto5.java
+++ Taller2/src/TallerCorte2/Punto5.java
@@ -14,11 +14,14 @@ public class Punto5 {
     
     public static void main(String[] args) {
         
+        //se declaran variables 
         int num1, num2; 
         
+        //se solicitan numeros al usuario
         num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer numero"));
         num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo numero"));
         
+        //se crea condicion para comprobar cual es el numero mayor y el numero menor 
         if (num1>num2){
         JOptionPane.showMessageDialog(null, "El numero mayor es:" +num1+ "Numero menor es:" +num2);
         }
