d7655115b440bcd1d7ee1ec5932ec67775e8ded6
DanielaLC011 <101845416+DanielaLC011@users.noreply.github.com>
2022-05-30 21:00:50 -0500

    Punto6
    Se agrega comentarios

--- Taller2/src/TallerCorte2/Punto6.java
+++ Taller2/src/TallerCorte2/Punto6.java
@@ -14,11 +14,14 @@ public class Punto6 {
     
     public static void main(String[] args) {
         
+        //Se declaran variables 
         int V1; 
         float VmS; 
         
+        //Se solicita el valor de Km por hora al usuario
         V1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la velocidad en Km/h"));
         
+        //Se determina la formula y se muestra el valor en metros por segundos 
         VmS = V1/3.6f; 
         JOptionPane.showMessageDialog(null, "La velocidad en m/s es:" +VmS);
      
